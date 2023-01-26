51b3c307172f6c6af3b1f5f0de77721969ef2227
Alejo <alejito23001@gmail.com>
2023-01-26 10:46:14 -0500

    NEW: Project Foundation

---
+++ backend/src/main/java/konnex/innovation/medicine_administration/MedicineAdministrationApplication.java
@@ -0,0 +1,13 @@
+package konnex.innovation.medicine_administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineAdministrationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/konnex/innovation/medicine_administration/MedicineAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package konnex.innovation.medicine_administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
